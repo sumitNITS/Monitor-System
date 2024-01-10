8310241c2e9861833d185e4846f10f6fe36b06ce
Sumit Kumar Das <krsumit449@gmail.com>
2024-01-10 15:30:15 +0530

    fixed error in stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-containerID = docker ps | awk -F " " '{print $1}'
-sudo docker rm -f $containerID
+containerID=$(docker ps | awk -F " " '{print $1}')
+sudo docker rm -f "$containerID"
